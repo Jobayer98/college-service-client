24ebb94389755298c6200eb7fc8b527b0962189e
Jobayer98 <jobayerrahman0220@gmail.com>
2023-07-22 08:07:17 +0600

    added footer section

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,23 +1,27 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./Header/NavBar";
 import SideBar from "./SideBar/SideBar";
+import Footer from "./Footer/Footer";
 
 const Root = () => {
   return (
-    <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        {/* Navbar */}
-        <NavBar />
-        {/* Page content here */}
-        <Outlet />
+    <>
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <NavBar />
+          {/* Page content here */}
+          <Outlet />
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+          {/* Sidebar */}
+          <SideBar />
+        </div>
       </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-        {/* Sidebar */}
-        <SideBar />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
