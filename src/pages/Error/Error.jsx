a1cd3e9a989d94e32a05f07d28289afe8ba97685
Jobayer98 <jobayerrahman0220@gmail.com>
2023-07-24 22:53:09 +0600

    add error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,10 @@
+import img from "../../assets/404.jpg";
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="">
+      <img className="inset-0 h-[100vh] w-full" src={img} alt="404" />
+    </div>
+  );
 };
 
 export default ErrorPage;
