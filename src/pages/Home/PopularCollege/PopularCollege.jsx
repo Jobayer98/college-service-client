46bc3c264481f71d54b897f12e6c066dd1c753a1
Jobayer98 <jobayerrahman0220@gmail.com>
2023-07-24 12:16:56 +0600

    added img url

--- src/pages/Home/PopularCollege/PopularCollege.jsx
+++ src/pages/Home/PopularCollege/PopularCollege.jsx
@@ -1,29 +1,79 @@
+import california from "../../../assets/city/california-min.webp";
+import texas from "../../../assets/city/texas-min.webp";
+import florida from "../../../assets/city/florida-min.webp";
+import newyork from "../../../assets/city/new-york-min.webp";
+import illinois from "../../../assets/city/illinois-min.webp";
+import pennsylvania from "../../../assets/city/pennsylvania-min.webp";
+import ohio from "../../../assets/city/ohio-min.webp";
+import alaska from "../../../assets/city/alaska-min.webp";
 const PopularCollege = () => {
-  const array = [1, 2, 3, 4, 5, 6, 7, 8];
+  const colleges = [
+    {
+      id: 1,
+      name: "California",
+      url: california,
+    },
+    {
+      id: 2,
+      name: "Texas",
+      url: texas,
+    },
+    {
+      id: 3,
+      name: "Florida",
+      url: florida,
+    },
+    {
+      id: 4,
+      name: "New York",
+      url: newyork,
+    },
+    {
+      id: 5,
+      name: "Illinois",
+      url: illinois,
+    },
+    {
+      id: 6,
+      name: "Pennsylvania",
+      url: pennsylvania,
+    },
+    {
+      id: 7,
+      name: "Ohio",
+      url: ohio,
+    },
+    {
+      id: 8,
+      name: "Alaska",
+      url: alaska,
+    },
+  ];
   return (
-    <section className="my-24 max-w-5xl mx-auto">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        Colleges in Popular City
-      </h1>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4">
-        {array.map((item) => (
-          <div
-            key={item}
-            className="border bg-gray-50 hover:shadow-xl rounded-lg"
-          >
-            <img
-              src="https://images.unsplash.com/flagged/photo-1554473675-d0904f3cbf38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvbGxlZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60"
-              className="w-full h-40 object-cover rounded-tl-lg rounded-tr-lg"
-            />
-            <div className="p-4">
-              <p className="text-gray-800 font-bold mb-2">
-                {"Colleges in Dhaka"}
-              </p>
+    <div className="bg-gray-100 p-1">
+      <section className="my-24 max-w-5xl mx-auto">
+        <h1 className="text-3xl font-bold mb-8">Colleges in Popular City</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4">
+          {colleges.map((college) => (
+            <div
+              key={college.id}
+              className="border bg-white hover:shadow-xl rounded-lg"
+            >
+              <img
+                src={college.url}
+                alt={college.name}
+                className="w-full h-28 object-cover rounded-tl-lg rounded-tr-lg"
+              />
+              <div className="p-4">
+                <p className="text-gray-800 font-bold mb-2">
+                  `Colleges in {college.name}`
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 
